3ae04ce9747413787d03d0c90cac23fe182a6757
safey <s-safey.yahia@zewailcity.edu.eg>
2023-12-04 21:41:45 +0200

    add derived classes WCP, SCP, NP Passengers and WCB, MB.

---
+++ SpecialCategoryPassengerClass.h
@@ -0,0 +1,15 @@
+//
+// Created by Safey Elrahman on 04/12/2023.
+//
+
+#ifndef EASYTRIP_SPECIALCATEGORYPASSENGERCLASS_H
+#define EASYTRIP_SPECIALCATEGORYPASSENGERCLASS_H
+
+#include "PassengerClass.h"
+
+class SpecialCategoryPassengerClass : PassengerClass{
+
+};
+
+
+#endif //EASYTRIP_SPECIALCATEGORYPASSENGERCLASS_H
---
+++ WheelChairBusClass.h
@@ -0,0 +1,16 @@
+//
+// Created by Safey Elrahman on 04/12/2023.
+//
+
+#ifndef EASYTRIP_WHEELCHAIRBUSCLASS_H
+#define EASYTRIP_WHEELCHAIRBUSCLASS_H
+
+#include "BusClass.h"
+
+
+class WheelChairBusClass : BusClass {
+
+};
+
+
+#endif //EASYTRIP_WHEELCHAIRBUSCLASS_H
