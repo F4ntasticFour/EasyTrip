5c5d022f5760a076902cf6b751f0d1abc0514629
safey <s-safey.yahia@zewailcity.edu.eg>
2023-12-04 21:39:00 +0200

    Station Class (feat): add station class header and source files

--- StationClass.h
+++ StationClass.h
@@ -2,6 +2,7 @@
 // Created by Safey Elrahman on 04/12/2023.
 //
 
+<<<<<<< HEAD
 #ifndef EASYTRIP_STATIONCLASS_H
 #define EASYTRIP_STATIONCLASS_H
 
@@ -17,16 +18,25 @@ public:
     PriorityQueue<PassengerClass> SPpassengers;
     LinkedList<PassengerClass> NPpassengers;
     LinkedList<PassengerClass> WPpassengers;
-    Station(int number);
-    void setStationNumber(int number);
-    int getStationNumber();
-    void addNpPassenger(PassengerClass passenger);
-    void addWpPassenger(PassengerClass passenger);
-    void addSpPassenger(PassengerClass SPpassenger);
-    void removeNpPassenger(PassengerClass passenger);
-    void removeWpPassenger(PassengerClass passenger);
-    void removeSpPassenger();
-};
 
+    Station(int number);
+
+    void setStationNumber(int number);
+
+    int getStationNumber();
+
+    void addNpPassenger(PassengerClass passenger);
+
+    void addWpPassenger(PassengerClass passenger);
+
+    void addSpPassenger(PassengerClass SPpassenger);
+
+    void removeNpPassenger(PassengerClass passenger);
+
+    void removeWpPassenger(PassengerClass passenger);
+
+    void removeSpPassenger();
+
+};
 
 #endif //EASYTRIP_STATIONCLASS_H
