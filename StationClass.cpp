75c240cdc97fec526e6d075fefe6b23f4a639547
abdulrahman <s-abd.khlail@zewailciy.edu.eg>
2023-12-05 11:56:19 +0200

    feat: implement Station Class

--- StationClass.cpp
+++ StationClass.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "StationClass.h"
+
+Station::Station(string name) {
+    this->name = name;
+}
+void Station::addNpPassenger(PassengerClass passenger) {
+    NPpassengers.insertAtEnd(passenger);
+}
+void Station::addWpPassenger(PassengerClass passenger) {
+    WPpassengers.insertAtEnd(passenger);
+}
+void Station::addSpPassenger(PassengerClass passenger) {
+    SPpassengers.enqueue(passenger, passenger.getPriority());
+}
+void Station::removeNpPassenger(PassengerClass passenger) {
+    NPpassengers.remove(passenger);
+}
+void Station::removeWpPassenger(PassengerClass passenger) {
+    WPpassengers.remove(passenger);
+}
+void Station::removeSpPassenger(PassengerClass passenger) {
+    SPpassengers.dequeue();
+}
+string Station::getName() {
+    return name;
+}
+void Station::setName(string name) {
+    this->name = name;
+}
--- StationClass.h
+++ StationClass.h
@@ -5,9 +5,27 @@
 #ifndef EASYTRIP_STATIONCLASS_H
 #define EASYTRIP_STATIONCLASS_H
 
+#include <string>
+#include "PriorityQueue.h"
+#include "PassengerClass.h"
+#include "LinkedList.h"
+using namespace std;
 
-class StationClass {
-
+class Station {
+public:
+    string name;
+    PriorityQueue<PassengerClass> SPpassengers;
+    LinkedList<PassengerClass> NPpassengers;
+    LinkedList<PassengerClass> WPpassengers;
+    Station(string name);
+    void setName(string name);
+    string getName();
+    void addNpPassenger(PassengerClass passenger);
+    void addWpPassenger(PassengerClass passenger);
+    void addSpPassenger(PassengerClass SPpassenger);
+    void removeNpPassenger(PassengerClass passenger);
+    void removeWpPassenger(PassengerClass passenger);
+    void removeSpPassenger(PassengerClass passenger);
 };
 
 
