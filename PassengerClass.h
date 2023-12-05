2d5ae8ec4e2380609e9e456c3c33a8ec29d986d4
marwanamr <maro99947@gmail.com>
2023-12-05 13:21:31 +0200

    feat: implement bus and passenger classes

--- PassengerClass.h
+++ PassengerClass.h
@@ -4,10 +4,38 @@
 
 #ifndef EASYTRIP_PASSENGERCLASS_H
 #define EASYTRIP_PASSENGERCLASS_H
+#include "TimeClass.h"
 
+#include <string>
 
 class PassengerClass {
+private:
+    Time ArrivalTime;
+    int StartStation;
+    int EndStation;
+    Time GetOnOffTime;
+    std::string PassengerType;
+    std::string statue;
+public:
+    // Constructor
+    PassengerClass(Time& ArrivalTime, const int& StartStation, int& EndStation, Time& GetOnOffTime, const std::string& passengerType);
 
+    // Getters
+    Time getArrivalTime() const;
+    int getStartStation() const;
+    int getEndStation() const;
+    Time getGetOnOffTime() const;
+    std::string getPassengerType() const;
+
+    // Setters
+    void setArrivalTime(const Time& arrivalTime);
+    void setStartStation(const int& startStation);
+    void setEndStation(const int& endStation);
+    void setGetOnOffTime(Time getOnOffTime);
+    void setPassengerType(const std::string& passengerType);
+    void setStatue(const std::string& statue);
+    int getPriority() const;
+    std::string getStatue() const;
 };
 
 
