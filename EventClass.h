3400ac3a095c72cf5e57189bd5b74867d5a763de
ustafaa <s-mostafa.abdalelah@zewailcity.edu.eg>
2023-12-09 01:54:11 +0200

    implementaion of event class with derived arriavl/leave classes with the overriden excute functionality

--- EventClass.h
+++ EventClass.h
@@ -2,10 +2,53 @@
 // Created by Safey Elrahman on 04/12/2023.
 //
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++11-extensions"
 #ifndef EASYTRIP_EVENTCLASS_H
 #define EASYTRIP_EVENTCLASS_H
+#include "TimeClass.h"
+#include "CompanyClass.h"
+class Event{
+protected:
+    char type;
+    Time eventTime;
+    int startStation;
+    int endStation;
+    int passengerID;
+    Time GetOnOffTime=Time(0,1/6);
+    CompanyClass *C;
 
-//event (base) sub class leave and arrive each has an seperate execute function
-//pointer to company
+public:
+    Event(Time eventTime, int passengerID, int startStation, int endStation, char type);
+    char getType();
+    void setTime(Time &eventTime);
+
+    Time getTime();
+
+    virtual void  execute()=0;
+};
+
+class ArriveEvent: public Event{
+protected:
+    CompanyClass *C;
+    Time ArrivalTime;
+
+public:
+    ArriveEvent(Time ArrivalTime,int passengerID, int startStation, int endStation);
+    void execute() override;
+};
+class LeaveEvent: public Event{
+protected:
+    CompanyClass *C;
+    Time LeaveTime;
+
+
+public:
+    LeaveEvent(Time LeaveTime,int passengerID, int startStation, int endStation);
+    void execute() override;
+};
+
 
 #endif //EASYTRIP_EVENTCLASS_H
+
+#pragma clang diagnostic pop
