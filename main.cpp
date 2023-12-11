c45b43ecb859d78af44ffa5254983cd3138de1f1
safey <s-safey.yahia@zewailcity.edu.eg>
2023-12-11 21:08:27 +0200

    (Fix): memory leak

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
 #include "EventClass.h"
 #include "StationClass.h"
 #include "BusClass.h"
+#include "FileHandler.h"
 #include "UiClass.h"
 
 using namespace std;
 
 int main() {
+    FileHandler fileHandler("../random_file.txt");
+    CompanyClass* company = new CompanyClass(fileHandler.getNumStations(), fileHandler.getTimeBetweenStations(),
+                                             fileHandler.getWBusCount(), fileHandler.getMBusCount(),
+                                             fileHandler.getWBusCapacity(), fileHandler.getMBusCapacity(),
+                                             fileHandler.getJ(), fileHandler.getC_WBus(), fileHandler.getC_MBus());
+    TimeClass time(0, 0);
+    processEvent("../random_file.txt",time, company);
 }
