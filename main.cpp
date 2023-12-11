6636763756b172c0a14d44504f082ce396eb6015
safey <s-safey.yahia@zewailcity.edu.eg>
2023-12-11 19:50:30 +0200

    Event Class (fix): add Time argument to processEvents

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 #include "EventClass.h"
 #include "StationClass.h"
 #include "BusClass.h"
+#include "FileHandler.h"
 #include "UiClass.h"
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FileHandler file("input.txt");
+    CompanyClass* company = new CompanyClass(file.getNumStations(), file.getTimeBetweenStations(), file.getWBusCount(),
+                                             file.getMBusCount(), file.getWBusCapacity(), file.getMBusCapacity(),
+                                             file.getJ(), file.getC_WBus(), file.getC_MBus());
+    TimeClass time(0, 0);
+    processEvent("../random_file.txt", company);
+    return 0;
 }
